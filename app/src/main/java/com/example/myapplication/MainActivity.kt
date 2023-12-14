a24c0a0cfd38d48ff4e1777f3a3d27a215ecd10b
s.savin <s.savin@vk.team>
2023-12-14 17:28:01 +0300

    Понял шо це таке binding, как она работает. Добавил сам класс в проект.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var BindingClass : ActivityMainBinding // наследуемся от нового класса
+
     override fun onCreate(activitySergo: Bundle?) {
+        BindingClass = ActivityMainBinding.inflate(layoutInflater) // заполняем класс всем необходимым
         super.onCreate(activitySergo)
-        setContentView(R.layout.activity_main)
-
-        val testId = findViewById<TextView>(R.id.textAnalPortals)
-
-        testId.text = "POMENYAL TEXT EBAAAT'"
+        setContentView(BindingClass.root) // показываем, чем хотим управлять - а именно всем activity_main
+        BindingClass.buttonSwitch.setOnClickListener { // тест кнопки
+            BindingClass.textAnalPortals.text = "Mama Rodnaya Che tVORY"
+        }
     }
-}
+
+    }
