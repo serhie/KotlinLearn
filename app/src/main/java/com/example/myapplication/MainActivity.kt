befd35fdcae9f32d8c305d114123a54536b0f28c
serhie <serhiesaw@gmail.com>
2023-12-15 15:08:49 +0300

    Сделал бездарный псевдокалькулятор

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,20 +2,35 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val a = 150
+    val b = 150
+
     lateinit var BindingClass : ActivityMainBinding // наследуемся от нового класса
 
     override fun onCreate(activitySergo: Bundle?) {
         BindingClass = ActivityMainBinding.inflate(layoutInflater) // заполняем класс всем необходимым
         super.onCreate(activitySergo)
         setContentView(BindingClass.root) // показываем, чем хотим управлять - а именно всем activity_main
-        BindingClass.buttonSwitch.setOnClickListener { // тест кнопки
-            BindingClass.textAnalPortals.text = "Mama Rodnaya Che tVORY"
+
+        BindingClass.sum.setOnClickListener {
+            val result = a + b
+            BindingClass.resultView.text = "$result - Отсоси у тракториста"
         }
+        BindingClass.minus.setOnClickListener {
+            val result = a - b
+            BindingClass.resultView.text = result.toString()
+        }
+        BindingClass.ymnojenie.setOnClickListener {
+            val result = a * b
+            BindingClass.resultView.text = result.toString()
+        }
+
     }
 
     }
